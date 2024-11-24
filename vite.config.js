d9c171fa93d390c42dd25ddf2c06fb28d1a7c750
Priya Ranjan Singh <mail@priyaranjan.net>
2024-11-24 19:43:07 +0530

    Add federation plugin & config for client app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'umbrella-research',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Research': './src/App',
+      },
+      shared: [],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
 })
